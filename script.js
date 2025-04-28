c377fd4be49e8f1ce9210ad5e2c4d2b9edcb230d
METE BERK <96863692+MTBERK@users.noreply.github.com>
2025-04-28 13:17:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const letter = document.getElementById('letter');
 let opened = false;
 envelope.addEventListener('click', () => {
   if (opened) return;
-  flap.style.transform = 'rotateX(-180deg)';
+  flap.style.transform = 'rotateX(-120deg)';
   setTimeout(() => {
     letter.classList.add('open');
     envelope.style.display = 'none';
-  }, 1000);
+  }, 700);
   opened = true;
 }); 
